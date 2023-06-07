3462443a80880a38f6d6e511b76d2c0accae70e1
Aiirizz <kikimaulana935@gmail.com>
2023-06-07 10:27:38 +0700

    add ui activity

---
+++ app/src/main/java/academy/bangkit/kultura/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package academy.bangkit.kultura.ui.detail
+
+import academy.bangkit.kultura.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
---
+++ app/src/main/java/academy/bangkit/kultura/ui/splash/SplashActivity.kt
@@ -0,0 +1,52 @@
+package academy.bangkit.kultura.ui.splash
+
+import academy.bangkit.kultura.R
+import academy.bangkit.kultura.databinding.ActivitySplashBinding
+import academy.bangkit.kultura.ui.home.HomeActivity
+import android.animation.ObjectAnimator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        val handler = Handler()
+        handler.postDelayed({
+
+            val intent = Intent(this@SplashActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+        splashAnimation()
+    }
+
+    private fun splashAnimation() {
+        ObjectAnimator.ofFloat(binding.logoSplash, View.TRANSLATION_Y, 0f, 0f).apply {
+            duration = 1500
+        }.start()
+
+        ObjectAnimator.ofFloat(binding.logoSplash, View.ALPHA, 0f, 1f).apply {
+            duration = 1500
+        }.start()
+
+        ObjectAnimator.ofFloat(binding.logoSplash, View.SCALE_X, 0f, 1f).apply {
+            duration = 1500
+        }.start()
+
+        ObjectAnimator.ofFloat(binding.logoSplash, View.SCALE_Y, 0f, 1f).apply {
+            duration = 1500
+        }.start()
+    }
+}
