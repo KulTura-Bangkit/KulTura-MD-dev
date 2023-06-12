e135c29abc4f27836415ea0285e5812024ad7f0d
Aiirizz <kikimaulana935@gmail.com>
2023-06-12 09:07:16 +0700

    add uploadImg and openGalery

--- app/src/main/java/academy/bangkit/kultura/ui/upload/UploadPhotoActivity.kt
+++ app/src/main/java/academy/bangkit/kultura/ui/upload/UploadPhotoActivity.kt
@@ -1,12 +1,121 @@
 package academy.bangkit.kultura.ui.upload
 
 import academy.bangkit.kultura.R
+import academy.bangkit.kultura.databinding.ActivityUploadPhotoBinding
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 class UploadPhotoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUploadPhotoBinding
+    private lateinit var photoPath : String
+    private var getFile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload_photo)
+        binding = ActivityUploadPhotoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCamera.setOnClickListener {
+            takePhoto()
+        }
+
+        binding.buttonGallery.setOnClickListener {
+            openGallery()
+        }
+
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun takePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@UploadPhotoActivity,
+                "academy.bangkit.kultura",
+                it
+            )
+            photoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(photoPath)
+            myFile.let { file ->
+                getFile = file
+                binding.imageView2.setImageBitmap(BitmapFactory.decodeFile(file.path))
+            }
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, getString(R.string.pick_picture))
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private fun createCustomTempFile(context: Context): File {
+        val storageLoc: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val time: String = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+        return File.createTempFile(time, "jpg", storageLoc)
+
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@UploadPhotoActivity)
+                getFile = myFile
+                binding.imageView2.setImageURI(uri)
+            }
+        }
+    }
+
+    private fun uriToFile(selectedImg: Uri, context: Context): File {
+        val contentResolver: ContentResolver = context.contentResolver
+        val myFile = createCustomTempFile(context)
+
+        val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+        val outputStream: OutputStream = FileOutputStream(myFile)
+        val buf = ByteArray(1024)
+        var len: Int
+        while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+        outputStream.close()
+        inputStream.close()
+
+        return myFile
+    }
+
+    companion object{
+        const val FILENAME_FORMAT = "dd-MMM-yyyy"
     }
 }
