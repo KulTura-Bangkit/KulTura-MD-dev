078c5199b5298213a97c7254f8bf6944fffec07c
Aiirizz <kikimaulana935@gmail.com>
2023-06-14 16:07:36 +0700

    add rv adapter

--- app/src/main/java/academy/bangkit/kultura/adapter/RecommendAdapter.kt
+++ app/src/main/java/academy/bangkit/kultura/adapter/RecommendAdapter.kt
@@ -1,4 +1,44 @@
 package academy.bangkit.kultura.adapter
 
-class RecommendAdapter {
+import academy.bangkit.kultura.R
+import academy.bangkit.kultura.ui.dashboard.UserResponse
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class RecommendAdapter(private val context: Context, private val listRec: ArrayList<UserResponse.Item>) : RecyclerView.Adapter<RecommendAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_recomendation, parent, false))
+    override fun onBindViewHolder(holder: RecommendAdapter.ViewHolder, position: Int) {
+        val rvItem = listRec[position]
+        holder.apply {
+            Glide.with(itemView.context)
+                .load(rvItem.url_image)
+                .into(tvImg)
+            tvName.text = rvItem.name
+
+            holder.itemView.setOnClickListener {
+
+            }
+        }
+    }
+
+    override fun getItemCount() = listRec.size
+
+
+    class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        val tvImg : ImageView = view.findViewById(R.id.rv_batik_img)
+        val tvName : TextView = view.findViewById(R.id.rv_batik_name)
+    }
+
+    fun setData(data: List<UserResponse.Item>){
+        listRec.clear()
+        listRec.addAll(data)
+        notifyDataSetChanged()
+    }
 }
